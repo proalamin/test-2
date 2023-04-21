4c523579d8f90ab2ec8bc04083ed40503fd36976
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-04-21 11:42:42 +0530

    Init Commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+const prod = process.env.NODE_ENV === 'production'
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+})
+module.exports = withBundleAnalyzer(withPWA({
+    pwa: {
+        disable: prod ? false : true,
+        dest: 'public',
+        runtimeCaching: prod ? runtimeCaching : undefined,
+    },
+}))
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './public/index.html'
+    ],
+    darkMode: false,
+    theme: {},
+    variants: {},
+    plugins: [],
+}
