41c672ba780393751cd9a19bcdc2ecf205d8237f
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-04-22 04:13:40 +0530

    Added Meta Tags For SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,6 @@ export default function Document () {
     return (
         <Html lang="en">
             <Head>
-                <meta name="robots" content="noindex"/>
-                <meta name="googlebot" content="noindex"/>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
                     rel="stylesheet"/>
