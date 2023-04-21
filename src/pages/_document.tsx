17b269f61af97fdb5a07dc68b2d12d757d9c961d
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-04-22 04:10:45 +0530

    Added Meta Tags For SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,17 @@ export default function Document () {
     return (
         <Html lang="en">
             <Head>
+                <meta name="robots" content="noindex"/>
+                <meta name="googlebot" content="noindex"/>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
                     rel="stylesheet"/>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, maximum-scale=5, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+                />
+                <meta name="description" content="Search My Expert"/>
+                <title>Search My Expert</title>
             </Head>
             <body>
             <Main/>
