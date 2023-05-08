868dcbdb3af442a388a635f953d168c471ad92da
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2023-05-08 17:45:39 +0530

    Update Build Issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,3 @@
-import React from 'react'
 import Home from "@/pages/home";
 
-export default function index () {
-    return <>
-        <div className="main">
-            <Home/>
-        </div>
-    </>
-}
+export default Home;
